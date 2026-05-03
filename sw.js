906550dff5808896ae6d561b60f6303a9c797850
Jirodara <mehdinama805@gmail.com>
2026-05-03 04:54:16 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const V='arna2';
-self.addEventListener('install',e=>{e.waitUntil(caches.open(V).then(c=>c.addAll(['./index.html','./manifest.json','./icon.svg'])));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==V).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{if(e.request.url.includes('binance')||e.request.url.includes('fonts'))return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
